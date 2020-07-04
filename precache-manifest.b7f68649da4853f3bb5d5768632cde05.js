0fd901147a85fdbac17975e8e490c6e5453856c6
Lucas Pettersson <clm.pettersson@gmail.com>
2020-07-04 23:49:23 +0200

    Updates

---
+++ precache-manifest.b7f68649da4853f3bb5d5768632cde05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67de50eda0e0d2dec763cd49ed3223d5",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "923814066719dba9d38f",
+    "url": "/random-quote-machine/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "95b1ded25ecbefb3b150",
+    "url": "/random-quote-machine/static/js/2.b866051b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/random-quote-machine/static/js/2.b866051b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "923814066719dba9d38f",
+    "url": "/random-quote-machine/static/js/main.148f1587.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  }
+]);
